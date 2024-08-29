9ba473c68113e2eb19c1d367e89a617141e118d1
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-08-28 22:57:50 -0700

    Define a default message handling function if none is passed into the consume method

--- teams/security_worker.py
+++ teams/security_worker.py
@@ -4,4 +4,4 @@ import os
 from worker import Worker
 
 security_worker = Worker("security.high.accident", "high.accident")
-security_worker.consume()
+security_worker.consume(None)
--- teams/worker.py
+++ teams/worker.py
@@ -8,6 +8,8 @@ class Worker:
         amqp_url = os.environ["AMQP_URL"]
         url_params = pika.URLParameters(amqp_url)
         connection = pika.BlockingConnection(url_params)
+
+        # Worker attributes
         self.channel = connection.channel()
         self.queue_name = queue_name
         self.queue_routing_key = queue_routing_key
@@ -20,19 +22,22 @@ class Worker:
             exchange=exchange, queue=queue_name, routing_key=queue_routing_key
         )
 
-    def consume(self):
-        def receive_msg(ch, method, properties, body):
-            print("received msg : ", body.decode("utf-8"))
-            print("acking it")
+    def consume(self, msg_handler):
+        def handle_msg_default(ch, method, properties, body):
+            print("received message: ", body.decode("utf-8"))
+            print("sending ack")
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
         # Make sure the consumer receives only one message at a time
         # next message is received only after acking the previous one
         self.channel.basic_qos(prefetch_count=1)
 
+        if msg_handler is None:
+            msg_handler = handle_msg_default
+
         # Define the queue consumption
         self.channel.basic_consume(
-            queue=self.queue_name, on_message_callback=receive_msg
+            queue=self.queue_name, on_message_callback=msg_handler
         )
 
         print("Waiting to consume")
