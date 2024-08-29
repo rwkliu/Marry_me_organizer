18973afd21413faca56f0f817f4d293523017f5f
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-08-28 23:02:54 -0700

    Add event handler to security worker, overwriting the default message handler in the worker's consume method

--- teams/security_worker.py
+++ teams/security_worker.py
@@ -3,5 +3,11 @@ import time
 import os
 from worker import Worker
 
+
+def handle_event(ch, method, properties, body):
+    print("This message was received: ", body.decode("utf-8"))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 security_worker = Worker("security.high.accident", "high.accident")
-security_worker.consume(None)
+security_worker.consume(handle_event)
