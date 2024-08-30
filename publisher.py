5fa48a64d02aa7a30be4b9f41da5b720691165f1
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-08-30 14:05:58 -0700

    Add loop to publish multiple messages

--- publisher.py
+++ publisher.py
@@ -6,12 +6,13 @@ channel = connection.channel()
 route_key = "high.accident"
 message = "high accident event"
 
-channel.basic_publish(
-    exchange="coordinator",
-    routing_key=route_key,
-    body=message,
-    properties=pika.BasicProperties(delivery_mode=2),
-)
-print("message sent!")
+for i in range(5):
+    channel.basic_publish(
+        exchange="coordinator",
+        routing_key=route_key,
+        body=message,
+        properties=pika.BasicProperties(delivery_mode=2),
+    )
+    print("message sent!")
 channel.close()
 connection.close()
