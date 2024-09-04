1a544b38e317c0d383c06328a6c60fbfdd0d1394
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-09-04 15:38:20 -0700

    Read dataset and publish the events to rabbitmq

--- publisher.py
+++ publisher.py
@@ -1,18 +1,43 @@
 import pika
+from json_parser import read_file, sort_contents
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
 
-route_key = "accident.security"
-message = "high accident event"
+file_name = "dataset_1.json"
+file_path = "./data/" + file_name
+events = sort_contents(read_file(file_path))
 
-for i in range(5):
-    channel.basic_publish(
-        exchange="high",
-        routing_key=route_key,
-        body=message,
-        properties=pika.BasicProperties(delivery_mode=2),
-    )
-    print("message sent!")
+event_routing_key_table = {
+    "brawl": "brawl.security",
+    "accident": "accident.security",
+    "not_on_list": "not_on_list.security",
+    "dirty_table": "dirty_table.cleanup",
+    "broken_items": "broken_items.cleanup",
+    "dirty_floor": "dirty_floor.cleanup",
+    "bad_food": "bad_food.catering",
+    "music": "music.catering",
+    "feeling_ill": "feeling_ill.catering",
+    "bride": "bride.officiant",
+    "groom": "groom.officiant",
+}
+
+channel.exchange_declare(exchange="high", exchange_type="topic")
+channel.exchange_declare(exchange="medium", exchange_type="topic")
+channel.exchange_declare(exchange="low", exchange_type="topic")
+
+for event in events:
+    exchange = event["priority"].lower()
+    routing_key = event_routing_key_table[event["event_type"]]
+    description = event["description"]
+    if exchange == "high":
+        channel.basic_publish(
+            exchange=exchange,
+            routing_key=routing_key,
+            body=description,
+            properties=pika.BasicProperties(delivery_mode=2),
+        )
+        print("event sent: ", exchange, routing_key, description)
+
 channel.close()
 connection.close()
